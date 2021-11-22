99f80eb62871ab0d9db61451a7ddb98137cdfd84
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2021-11-23 02:17:39 +0900

    Create ArrayTest.java

---
+++ 제너릭클래스와 컬렉션/ReverseArrayProject/ArrayTest.java
@@ -0,0 +1,12 @@
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		Double[] darr = {1.0, 2.0, 3.0, 4.0, 5.0};
+		String[] strs = { "jave", "c", "python"};
+		Array.<Double>reverse(darr);
+		Array.<String>reverse(strs);
+		// Array.<Double>reverse(new Double[] {1.0, 2.0, 3.0, 4.0, 5.0});
+	}
+
+}
