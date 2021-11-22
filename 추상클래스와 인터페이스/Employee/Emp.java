2e758802a4f4cc38205efa8b7a70a3f1904885ca
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2021-11-23 02:34:56 +0900

    Create Emp.java

---
+++ 추상클래스와 인터페이스/Employee/Emp.java
@@ -0,0 +1,12 @@
+
+public class Emp {
+	private int salary;
+	String dept = "Office";
+	
+	public void working() {
+		System.out.println("Employee Working");
+	}
+	public void working(String dept) {
+		System.out.println("E W at T O");
+	}
+}
