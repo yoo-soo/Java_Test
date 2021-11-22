48666f9c68b07292beae244499d952b18105893c
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2021-11-23 02:29:49 +0900

    Create Calculator.java

---
+++ 추상클래스와 인터페이스/PackageExample/Calculator.java
@@ -0,0 +1,8 @@
+package my.lib;
+
+abstract public class Calculator {
+	public abstract int add(int a, int b);
+	public abstract int subtract(int a, int b);
+	public abstract double average(int a, int b);
+	}
+
