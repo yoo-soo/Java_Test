dc793ad4b35b2ad920385ba803abe6cd4d5b6536
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2021-11-23 02:34:13 +0900

    Create Man.java

---
+++ 추상클래스와 인터페이스/Employee/Man.java
@@ -0,0 +1,12 @@
+
+public class Man extends Emp{
+	int allowance;
+	
+	public void working() {
+		System.out.println("M W");
+	}
+	
+	public void working(String dept){
+		System.out.println("M W at T O");
+	}
+}
