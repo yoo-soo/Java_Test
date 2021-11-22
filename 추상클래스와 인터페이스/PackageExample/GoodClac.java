e1d12bad7628f4d7a9d66f675c4d2b18de43783a
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2021-11-23 02:29:33 +0900

    Create GoodClac.java

---
+++ 추상클래스와 인터페이스/PackageExample/GoodClac.java
@@ -0,0 +1,23 @@
+package my.lib;
+
+public class GoodCalc extends Calculator {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public int subtract(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public double average(int a, int b) {
+		// TODO Auto-generated method stub
+		return ( a+b / 2 );
+	}
+
+}
