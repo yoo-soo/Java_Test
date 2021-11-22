0c857c0e79bfc2d34b77c23ca55f12e76a0fe87e
SOOKYUNG YOO <80819675+yoo-soo@users.noreply.github.com>
2021-11-23 02:29:15 +0900

    Create CalTest.java

---
+++ 추상클래스와 인터페이스/PackageExample/CalTest.java
@@ -0,0 +1,13 @@
+package app;
+
+import my.lib.*;
+public class CalTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GoodCalc c = new GoodCalc();
+		System.out.println( c.add(10, 20) );
+		System.out.println( c.subtract(20, 5) );
+	}
+
+}
